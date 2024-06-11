eea648b577f926a41408a9a5f28028f8b693ac4f
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2024-06-11 22:46:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function firstNonRepeatedChar(str) {
-	const charCount = {}
-	for (let char of str){
+	const charCount = {};
+	for(let char of str){
+		// if(!charCount[char]){
+		// 	charCount[char] = 1;
+		// } else {
+		// 	charCount[char]++;
+		// }
 		charCount[char] = (charCount[char] || 0) + 1;
 	}
+	
 	for(let char of str){
-		if(charCount[char] === 1){
-			return char;
+		if(charCount[char] === 1){ 
+			return char
 		}
 	}
 	return null;
