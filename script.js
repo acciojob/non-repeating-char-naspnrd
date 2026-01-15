b21bcdd260b2a9c79044fe0c6b52b8aa29000791
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2026-01-15 21:01:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+
+	let charObj = {
+    // y:2
+}
+// console.log(str[0])
+
+for(let s of str){
+    
+if(charObj[s]){
+    charObj[s] += 1
+}
+else charObj[s] = 1
+}
+
+console.log(charObj)
+
+for(let char in charObj){
+    // console.log(char)
+    if(charObj[char] == 1){
+        return char
+    }
+}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
